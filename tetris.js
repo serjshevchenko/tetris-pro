c122cb79d69fd29094e3a0e7a6a743a8bee7bfd2
serj.shevcheko <serj.shevcheko@gmail.com>
2014-08-31 11:48:04 +0300

    [refactoring] step 1

--- tetris.js
+++ tetris.js
@@ -11,7 +11,7 @@
 
 
 var defOptions = {
-		speed : 200, 		//in msec
+		speed : 150, 		//in msec
 		color : '#fff' 		// free cell
 	};
 
@@ -30,7 +30,6 @@ var Tetris = function (options) {
         this.prepareLayout().initModels().addListeners();
 		
 		//IT'S SHOW'S TIME
-		this.finish = false;
 		this.run();
     };    
     
@@ -38,9 +37,8 @@ Tetris.prototype = {
                 numVer : 20,
                 numHor : 15,
                 
-                finish : true,
-                
                 models : [],
+                cModel : null,
                 
                 wrapper : null,
                 field   : null,
@@ -67,14 +65,26 @@ Tetris.prototype = {
                                         var cell = document.createElement('DIV');
                                         cell.className = 'cell c-'+(j+1);
                                         cell.__busy = false;
-										cell.__isFree = function () {											
+										cell.__model = null;
+										cell.isFree = function () {											
 											return !this.__busy;
 										};
-										cell.__reserve = function (cell) {
+										cell.reserve = function (model) {
 											this.__busy = true;
+											this.__model = model;
+											return this;
 										};
-										cell.__release = function () {
-											this.__busy = false;				
+										cell.release = function () {
+											this.__busy = false;
+											this.__model = null;
+											return this;			
+										};
+										cell.getModel = function () {
+												return this.__model;
+										};
+										cell.changeColor = function (color) {
+											this.style.backgroundColor = color;
+											return this;
 										};
                                         row.appendChild(cell);
                                 }
@@ -119,10 +129,8 @@ Tetris.prototype = {
 				},
 				
 				run : function () {
-					if (!this.finish) {
-						var model = this.getRandModel();
-						model.go();
-					}
+					this.cModel = this.getRandModel();
+					this.cModel.go();
 					return this;
 				},
                 
@@ -142,7 +150,6 @@ Tetris.prototype = {
                 
                 addListeners : function () {
                         var self = this;
-
                         //~ this.btnStart.onclick = function (event) {
                         //~ };
                         //~ 
@@ -151,24 +158,47 @@ Tetris.prototype = {
 //~ 
                         //~ this.btnStop.onclick = function (event) {
                         //~ };
-                        
+                        window.onkeydown = function (e) {
+							//~ console.log(e.keyCode);
+							var dict = {
+									37 : 'Left',
+									39 : 'Right',
+									40 : 'Down',
+									38 : 'Up'
+							};
+							var action = 'on'+dict[e.keyCode];
+							self.cModel[action] && self.cModel[action]();
+						};
                         
                         EventManager.on('model.stop', function () {
 							setTimeout(function () {
 								self.run();
 							}, self.getOption('speed'));
 						});
-
+						
 						return this;
                 },
+                
+                bind : function (elem, event, handler) { // specify event arg with out prefix 'on'
+					if (!event || event in elem || handler.constructor != Function) {
+						return;
+					}
+					if (elem.addEventListener) {
+						elem.addEventListener(event, handler, false);
+					} else if (elem.attachEvent) {
+						elem.attachEvent('on'+event, handler);
+					} else {
+						elem['on'+event] = handler;
+					}
+				},
                 
                 getCell : function (row, cell) {
 					if (row == undefined || cell == undefined) {
 						throw new Error('Arguments error');
 					}
-					var row = this.field.getElementsByClassName ('r-'+row)[0];
+					var row = this.field.getElementsByClassName('r-'+row)[0];
 					if (row) {
-						var cell = row.getElementsByClassName ('c-'+cell)[0];
+						var cell = row.getElementsByClassName('c-'+cell)[0];
 						if (cell) {
 							return cell;
 						}
@@ -185,70 +215,112 @@ var Model = function (foreman) {
 		this.rotationRight = function () {};
 		this.rotationLeft = function () {};
 		this.position = [];
+		this.cells = {};
 		if (!foreman) {
 			throw new Error('Argument foreman is empty');
 		}
 		this.foreman = foreman;
-		this.flush = function (mode) { // if mode == false it's means to hide model
-			for (var len = this.position.length, i = 0; i < len; i++) {
-				x = this.position[i].x;
-				y = this.position[i].y;
-				var cell = this.foreman.getCell(y, x);
-				if (cell) {
-					if (mode === false) {
-						cell.style.backgroundColor = this.foreman.getOption('color');
-						cell.__release();
-					} else {
-						cell.style.backgroundColor = this.color;
-						cell.__reserve();
-					}
+		
+		this.flush = function (arr) { // if mode == false it's means to hide model
+			arr = arr || [];
+			
+			for (var i = 0; i < arr.length; i++) {
+				arr[i].changeColor('white').release();
+			}
+			arr = [];
+			for (i = 0; i < this.position.length; i++) {
+				var 
+					x = this.position[i].x,
+					y = this.position[i].y,
+					cell = this.foreman.getCell(y, x)
+				;
+				if (cell && (cell.isFree() || cell.getModel() === this)) {
+					arr.push(cell);
+					this.position[i].y += 1;
+				} else {
+					break;
 				}
 			}
-			return this;
+			if (i == this.position.length) {
+				for (i = 0; i < arr.length; i++) {
+					arr[i].reserve(this).changeColor(this.color);
+				}
+				var self = this;
+				setTimeout(function () {
+					self.flush(arr);
+				}, this.foreman.getOption('speed'));
+			}
+		};
+		
+		this.initCells = function () {
+			for (var i = 0; i < this.position.length; i++) {
+				var 
+					x = this.position[i].x,
+					y = this.position[i].y
+				;
+				var cell = this.foreman.getCell(x, y);
+				this.cells[x+':'+y] = cell;
+				cell.model = this;
+			}
 		};
 		
 		this.go = function () {
 			var self = this;
-			if (this.isFreeSpace()) {
-				this.flush();
-				setTimeout(function () {
-						self.move();
-				}, this.foreman.getOption('speed'));
-			}
+			//~ if (this.isFreeSpace()) {
+			this.flush();
+				//~ setTimeout(function () {
+						//~ self.move();
+				//~ }, this.foreman.getOption('speed'));
+			//~ }
 			return this;
 		};
 		
-		this.move = function () {
-			var end = false;
-			this.flush(false);
-			for (var len = this.position.length, i = 0; i < len; i++) {
-				this.position[i].y += 1;
-				var cell = this.foreman.getCell(this.position[i].y + 1, this.position[i].x);
-				if (!cell || !cell.__isFree()) {
-					end = true;
-				}
-			}
-			this.flush();
-			if (end) {
-				EventManager.fire('model.stop');
-				return;
-			}
-			var self = this;
-			setTimeout(function () {
-				self.move();
-			}, this.foreman.getOption('speed')); // TODO get interval from options			
+		//~ this.move = function () {
+			//~ var end = false;
+			//~ this.flush(false);
+			//~ for (var len = this.position.length, i = 0; i < len; i++) {
+				//~ this.position[i].y += 1;
+				//~ var cell = this.foreman.getCell(this.position[i].y + 1, this.position[i].x);
+				//~ if (!cell || !cell.isFree()) {
+					//~ end = true;
+				//~ }
+			//~ }
+			//~ this.flush();
+			//~ if (end) {
+				//~ EventManager.fire('model.stop');
+				//~ return;
+			//~ }
+			//~ var self = this;
+			//~ setTimeout(function () {
+				//~ self.move();
+			//~ }, this.foreman.getOption('speed')); // TODO get interval from options			
+		//~ };
+		
+		//~ this.isFreeSpace = function () {
+			//~ for (var len = this.position.length, i = 0; i < len; i++) {
+				//~ var cell = this.foreman.getCell(this.position[i].y, this.position[i].x);
+				//~ if (!cell || !cell.isFree()) {
+					//~ return false;
+				//~ }
+			//~ }
+			//~ return true;
+		//~ };
+		
+		this.onLeft = function () {
+			console.log('left');
 		};
 		
-		this.isFreeSpace = function () {
-			for (var len = this.position.length, i = 0; i < len; i++) {
-				var cell = this.foreman.getCell(this.position[i].y, this.position[i].x);
-				if (!cell || !cell.__isFree()) {
-					return false;
-				}
-			}
-			return true;
+		this.onRight = function () {
+			console.log('right');
 		};
 		
+		this.onDown = function () {
+			console.log('down');
+		};
+		
+		this.onUp = function () {
+			console.log('up');
+		};
 
     };
     
@@ -257,48 +329,56 @@ var Stick = function () {
 		Stick.prototype.superclass.apply(this, arguments);
 		this.color = '#0f0';
 		this.position = [{x:8,y:4}, {x:8,y:3}, {x:8,y:2}, {x:8,y:1}];
+		this.initCells();
 	};
 
 var Square = function () {
 		Square.prototype.superclass.apply(this, arguments);
-		this.color = '#00f';
+		this.color = '#0a0afe';
 		this.position = [{x:9,y:2}, {x:9,y:1}, {x:8,y:2}, {x:8,y:1}];
+		this.initCells();
 	};
 	
 var ZedLeft = function () {
 		ZedLeft.prototype.superclass.apply(this, arguments);
 		this.color = '#0003fe';
 		this.position = [{x:9,y:2}, {x:8,y:2}, {x:8,y:1}, {x:7,y:1}];
+		this.initCells();
 	};
    
 var ZedRight = function () {
 		ZedRight.prototype.superclass.apply(this, arguments);
 		this.color = '#01a3ff';
 		this.position = [{x:7,y:2}, {x:8,y:2}, {x:8,y:1}, {x:9,y:1}];
+		this.initCells();
 	};
 	
 var TetUp = function () {
 		TetUp.prototype.superclass.apply(this, arguments);
 		this.color = '#af48da';
 		this.position = [{x:7,y:2}, {x:9,y:2}, {x:8,y:2}, {x:8,y:1}];
+		this.initCells();
 	};
 	
 var TetDown = function () {
 		TetDown.prototype.superclass.apply(this, arguments);
 		this.color = '#ef4e1f';
 		this.position = [{x:7,y:1}, {x:9,y:1}, {x:8,y:2}, {x:8,y:1}];
+		this.initCells();
 	};
 	
 var GRight = function () {
 		GRight.prototype.superclass.apply(this, arguments);
 		this.color = '#aadd1f';
 		this.position = [{x:9,y:3}, {x:8,y:3}, {x:8,y:2}, {x:8,y:1}];
+		this.initCells();
 	};
 
 var GLeft = function () {
 		GLeft.prototype.superclass.apply(this, arguments);
 		this.color = '#521e98';
 		this.position = [{x:7,y:3}, {x:8,y:3}, {x:8,y:2}, {x:8,y:1}];
+		this.initCells();
 	};
 
 Stick.prototype.superclass = Model;
@@ -315,7 +395,6 @@ var EventManager = {
 			//~ 'model.prestart' : [],
 			//~ 'model.start' : [],
 			//~ 'model.move' : [],
-			//~ 'finish' : [],
 			'model.stop' : []
 		},
 		
